301f59c223f43f29fa4a4309cb0be3a20abe20cc
cyberjacob <cyberjacob@gmail.com>
2020-12-03 19:14:33 +0000

    Tidy day 3

--- 3/3.py
+++ 3/3.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 Tree = '#'
 
@@ -5,34 +6,28 @@ Tree = '#'
 def first(across=3, down=1):
     file = [line.strip() for line in open("input.txt")]
 
-    current_across = 0
-    current_down = 0
+    current_across = across
+    current_down = down
     trees = 0
 
-    while True:
+    while current_down + 1 <= len(file):
+        if file[current_down][current_across] == Tree:
+            trees += 1
+
         current_across += across
         current_down += down
 
-        if current_across + 1 > len(file[0]):
-            current_across -= len(file[0])
-
-        if current_down + 1 > len(file):
-            break
-
-        if file[current_down][current_across] == Tree:
-            trees += 1
+        current_across %= len(file[0])
 
     return trees
 
 
 def second():
-    one   = first(1, 1)
-    two   = first(3, 1)
-    three = first(5, 1)
-    four  = first(7, 1)
-    five  = first(1, 2)
+    pairs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    results = [first(across, down) for across, down in pairs]
+    result = reduce((lambda x, y: x * y), results)
 
-    return one * two * three * four * five
+    return result
 
 
 if __name__ == '__main__':
